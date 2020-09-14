fa48141853e35a4b445067510984e0f2ad772a5b
prithivraj <prithivraj007@gmail.com>
2020-09-14 19:40:23 +0100

    added dialog

--- logprinter-lib/src/main/java/com/intercom/logprinter_lib/CustomLogPrinter.kt
+++ logprinter-lib/src/main/java/com/intercom/logprinter_lib/CustomLogPrinter.kt
@@ -1,9 +1,15 @@
 package com.intercom.logprinter_lib
 
+import android.content.Context
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 
 class CustomLogPrinter {
     fun printLog() {
         Log.d("logprinter", "Printing from library")
+    }
+
+    fun showDialog(context: Context) {
+        AlertDialog.Builder(context).setView(R.layout.custom_fragment_layout).show()
     }
 }
