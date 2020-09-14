3809b4b2abfd5555d7b070ef3c3849be8db1655c
prithivraj <prithivraj007@gmail.com>
2020-09-14 19:22:23 +0100

    Initial commit

---
+++ app/src/main/java/com/intercom/logprinter/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.intercom.logprinter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intercom.logprinter_lib.CustomLogPrinter
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        CustomLogPrinter().printLog()
+    }
+}
---
+++ logprinter-lib/src/main/java/com/intercom/logprinter_lib/CustomLogPrinter.kt
@@ -0,0 +1,9 @@
+package com.intercom.logprinter_lib
+
+import android.util.Log
+
+class CustomLogPrinter {
+    fun printLog() {
+        Log.d("logprinter", "Printing from library")
+    }
+}
